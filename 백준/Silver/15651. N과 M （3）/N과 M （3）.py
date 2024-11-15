1a40ea5c6909f1c5d622a8913e5a18767203a9a3
조유진 <79037318+zinzo1019@users.noreply.github.com>
2024-11-15 22:32:02 +0900

    Update N과 M （3）.py
    refactor: 주석 수정

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -2,12 +2,13 @@ from itertools import product
 
 n, m = map(int, input().split())
 
-# Generate a sorted list from 1 to n
+# 1부터 n까지 리스트 생성
 nList = list(range(1, n + 1))
 
-# Use a set to track seen combinations and print unique results directly
+# 중복 제거를 위한 set 생성
 seen = set()
 
+# product는 주어진 리스트의 요소를 사용해 모든 가능한 조합을 생성한다.
 for combi in product(nList, repeat=m):
     if combi not in seen:
         seen.add(combi)
