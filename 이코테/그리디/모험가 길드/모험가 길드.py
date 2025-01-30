c3ad7daec66b2eeefe0ce6773bd950d7bab05460
조유진 <zinzo1019@gmail.com>
2025-01-30 14:29:01 +0900

    feat(모험가 길드): 모험가 길드 문제 풀이 업로드

--- 이코테/그리디/모험가 길드/모험가 길드.py
+++ 이코테/그리디/모험가 길드/모험가 길드.py
@@ -1,23 +1,27 @@
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=' ')
+'''
+5
+2 3 1 2 2
+'''
 
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+n = int(input())
+list = list(map(int, input().split()))
 
-graph = [
-	[],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-	[7],
-    [2,6,8],
-    [1,7]
-]
+# 오름차순
+list.sort(reverse=False)
 
-list = []
-visited = [False] * 9
-dfs(graph, 1, visited)
+num = 1
+result = 0
+
+for l in list:
+    # 그룹 완성
+    if num == l:
+        result += 1
+        # 그룹 초기화
+        num = 1
+
+    # 그룹 완성 X
+    # 그룹에 한 명 추가
+    else:
+        num += 1
+
+print(result)
